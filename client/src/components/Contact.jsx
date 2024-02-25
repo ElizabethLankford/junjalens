42a37b228600fa9300cdd89e8edd2dc6300af7cd
ElizabethLankford <elizabethlankford1@gmail.com>
2024-02-25 03:14:12 -0600

    contact base

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,4 +1,26 @@
 function Contact() {
-  return <div>Contact page...</div>;
+  return (
+    <div>
+      Contact page...
+      <form className="contact-form">
+        <label>
+          Name:
+          <input autoFocus required />
+        </label>
+        <label>
+          Email:
+          <input type="email" />
+        </label>
+        <label>
+          Subject:
+          <input />
+        </label>
+        <label>
+          Message:
+          <textarea />
+        </label>
+      </form>
+    </div>
+  );
 }
 export default Contact;
